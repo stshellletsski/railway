3916294b5533f34cd5e7b45d8336ebfdb5caf005
Marcin Strzelecki <118393661+stshellletsski@users.noreply.github.com>
2023-12-10 00:09:33 +0000

    Update welcome message to production

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended : true}));
 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { fact: `Hey! I'm Marcin, I find stats, facts and numbers very interesting.\r\nI code, teach and learn everyday.\r\nCredits:\r\nhttps://pattern.monster/\r\nAlbin Larsson - https://byabbe.se/\r\n`});
+    res.render("index.ejs", { fact: `Hey! I'm Text 2 fact! generator.\r\nProvide your text or leave this one and smash #Big Brainz to generate a fact from Wikipedia!\r\n\nI am currently looking to gain valuable experience\r\nIf you looking for junior dev, hit me up @ stshellletsski@gmail.com\r\n\nCredits:\r\nhttps://pattern.monster/\r\nAlbin Larsson - https://byabbe.se/\r\n`});
 });
 
 app.post("/get", async (req, res) => {
