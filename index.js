3e3d88de7816ec789566fe7495c467d92334f52c
Marcin Strzelecki <118393661+stshellletsski@users.noreply.github.com>
2023-12-10 00:20:23 +0000

    Update to production

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     res.render("index.ejs", { fact: `Hey! I'm Text 2 fact! generator.\r\nProvide your text or leave this one and smash #Big Brainz to generate a fact from Wikipedia!\r\n\nI am currently looking to gain valuable experience\r\nIf you are looking for junior dev, message me: stshellletsski@gmail.com\r\n\nCredits:\r\nhttps://pattern.monster/\r\nAlbin Larsson - https://byabbe.se/\r\n`});
 });
 
-app.post("/get", async (req, res) => {
+app.post("/generate", async (req, res) => {
     let input = req.body.text.replace(/\W/g, '').split("").map(x=>x.charCodeAt(0)).join(""); 
         
     let date;
